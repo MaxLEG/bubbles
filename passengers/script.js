5225caa4bed50b121834ff7d92cc01cff9abc438
MaxLEG <soldier322@gmail.com>
2025-07-24 22:17:07 +0200

    calculator v1.0

---
+++ passengers/script.js
@@ -0,0 +1,34 @@
+let passengers = [
+  { name: "Jane Doloop", paid: true },
+  { name: "Dr. Evel", paid: true },
+  { name: "Sue Property", paid: false },
+  { name: "John Funcall", paid: true },
+];
+
+function checkNoFlyList(passenger) {
+  return passenger.name === "Dr.Evil";
+}
+
+function checkNotPaid(passenger) {
+  return !passenger.paid;
+}
+
+function processPassengers(passengers, testFunction) {
+  for (let i = 0; i < passengers.length; i++) {
+    if (testFunction(passengers[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let allCanFly = processPassengers(passengers, checkNoFlyList);
+if (!allCanFly) {
+  console.log(
+    "The plane can't take off: we have a passenger on the no-fly-list."
+  );
+}
+let allPaid = processPassengers(passengers, checkNotPaid);
+if (!allPaid) {
+  console.log("The plane can't take off: not everyone has paid.");
+}
